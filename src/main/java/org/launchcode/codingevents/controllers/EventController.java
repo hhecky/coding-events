79174d9a9d2eacb977cb19bcd3bbd36006641a4d
Heather Hecky <hhecky@gmail.com>
2020-10-07 20:34:53 -0500

    finished exercises but css wasn't working

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -17,13 +19,20 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
-        return "events/index";
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+//        model.addAttribute("events", events);
+//        return "events/index";
+
+        Map<String, String> events = new HashMap<>();
+        events.put("Code with Pride", "LGBT Friendly Coding");
+        events.put("StrangeLoop", "loops that are strange");
+        events.put("Apple WWDS", "event for people who");
+        model.addAttribute("events", events); //gets hashmap data into template
+        return "events/index"; //path, go to events folder, find template called index and use that
     }
 
 }
